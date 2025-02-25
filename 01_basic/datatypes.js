b0c4a5bf845c02aa9db665b0818b2774139be3b3
Premraj_Jangam <109972416+premrajjangam@users.noreply.github.com>
2025-02-25 12:42:41 +0000

    datatypes

---
+++ 01_basic/datatypes.js
@@ -0,0 +1,10 @@
+let name = "prem"
+let age = 23;
+let isLoggedIn = false
+
+// number ==> 2 to power 53
+// BigInt
+// string ==> ""
+// boolean ==> true/false
+// null ==> standaloan value
+// undefined ==>
