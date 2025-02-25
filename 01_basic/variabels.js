8da625bf002c06e1a4970460004e90056508bbbe
Premraj_Jangam <109972416+premrajjangam@users.noreply.github.com>
2025-02-25 06:05:26 +0000

    variabels

---
+++ 01_basic/variabels.js
@@ -0,0 +1,8 @@
+const companyId  = 12212
+var companyName = "Prem"
+let companyNumber = 9898388388
+let companyCity
+
+console.log(companyId);
+
+console.log([companyName, companyNumber, companyCity]);
